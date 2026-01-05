cd04df82dd5cbb58976837ca11182129423257a9
rahatalmas <rahat.almas5599@gmail.com>
2026-01-05 08:45:26 +0600

    removed user-company-table

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,33 @@
+// src/modules/auth/auth.module.ts
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { AuthRepository } from './auth.repository';
+import { Company } from '../company/entities/company.entity';
+import { User } from '../user/entities/user.entity';
+import { JwtStrategy } from 'src/common/strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Company, User]), // ← Only Company and User
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'hello world',
+        signOptions: {
+          expiresIn: '24h',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, AuthRepository, JwtStrategy],
+  exports: [AuthService, AuthRepository, JwtStrategy, PassportModule, JwtModule],
+})
+export class AuthModule {}
