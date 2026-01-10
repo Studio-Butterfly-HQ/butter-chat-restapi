9a653360ae69dc94795bf82d774681f826899a23
rahatalmas <rahat.almas5599@gmail.com>
2026-01-11 05:08:25 +0600

    entityandrelation-updated

---
+++ src/modules/department/dto/create-department.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateDepartmentDto {
+    @ApiProperty({
+        example:'team-alpha',
+        description:'department-name'
+    })
+    @IsString()
+    @IsNotEmpty()
+    department_name: string
+
+    @ApiProperty({
+        example:'engineering and coding',
+        description:'description about the department'
+    })
+    @IsString()
+    @IsOptional()
+    description?:string
+}
